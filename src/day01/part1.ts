e63153491bc907c714e43048f813ecb711dd272a
Gabe Gonzalez <gabegon105@gmail.com>
2023-12-01 16:22:47 -0500

    day01 puzzle complete

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -1,5 +1,16 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input: string): number {
-  return 0;
+  // split input to an array by '\n'
+  // map to find first and last integer in each string
+  // reduce to find the sum
+  return input.split('\n').map( (str) => {
+    // split str to an array by ''
+    // filter for numbers
+    const numsArr = str.split('').filter(Number);
+
+    // return the first and last value of numsArr
+    return +(numsArr[0] + numsArr[numsArr.length-1]);
+  } )
+  .reduce( (sum,num) => sum + num, 0 );
 }
